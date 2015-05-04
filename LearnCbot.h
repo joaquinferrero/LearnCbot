4d5bed448b525a5e0e1d3c1d0a7a7726b4a8da42
joaquinferrero <explorer@joaquinferrero.com>
2015-05-05 00:09:28 +0200

    simplified version

--- LearnCbot.h
+++ LearnCbot.h
@@ -4,15 +4,21 @@
 //
 // Joaquin Ferrero (aka explorer)
 //
-// 2015-04-24T03:40:39+0200	: Primera versión
-// 2015-04-24T11:36:13+0200	: Corregir nombres
-// 2015-04-25T02:17:58+0200	: Reardenar secciones
+// 2015-04-24	: Primera versión
+// 2015-04-24	: Corregir nombres
+// 2015-04-25	: Reardenar secciones
+// 2015-05-02	: Más simple
 //
 
 #ifndef LearnCbot_h
 #define LearnCbot_h
 
-#include <Arduino.h>
+// (un)define LCbot_CANONICO two versions the code
+// LCbot_CANONICO defined: use Arduino API
+// LCbot_CANONICO not defined: use port calls directly
+#undef LCbot_CANONICO
+
+#include "Arduino.h"
 
 // Pousspoir - Buttons - Botones - Botãos -------------------------------------
 
@@ -21,45 +27,19 @@
 #define Pous2Pin		PIN3
 
 // Actions
+#ifdef LCbot_CANONICO
 #define Pous(x)			digitalRead(x)
+#else
+#define Pous(x)			((PIND & (1<<(x))) != 0)
+#endif
 
-// Français
+// Pousspoir
 #define Pous1			Pous(Pous1Pin)
 #define Pous1On			(Pous1 == LOW )
 #define Pous1Off		(Pous1 == HIGH)
 #define Pous2			Pous(Pous2Pin)
 #define Pous2On			(Pous2 == LOW )
 #define Pous2Off		(Pous2 == HIGH)
-
-// English
-#define Button1			Pous1
-#define Button1Pin		Pous1Pin
-#define Button1On		Pous1On
-#define Button1Off		Pous1Off
-#define Button2			Pous2
-#define Button2Pin		Pous2Pin
-#define Button2On		Pous2On
-#define Button2Off		Pous2Off
-
-// Español
-#define Boton1			Pous1
-#define Boton1Pin		Pous1Pin
-#define Boton1On		Pous1On
-#define Boton1Off		Pous1Off
-#define Boton2			Pous2
-#define Boton2Pin		Pous2Pin
-#define Boton2On		Pous2On
-#define Boton2Off		Pous2Off
-
-// Português
-#define Botao1			Pous1
-#define Botao1Pin		Pous1Pin
-#define Botao1On		Pous1On
-#define Botao1Off		Pous1Off
-#define Botao2			Pous2
-#define Botao2Pin		Pous2Pin
-#define Botao2On		Pous2On
-#define Botao2Off		Pous2Off
 
 // LEDs -----------------------------------------------------------------------
 
@@ -77,35 +57,35 @@
 #define LedVert1		Led2Pin
 #define LedVert2		Led3Pin
 
-// Actions
+// Actions PORTD
+#ifdef  LCbot_CANONICO
 #define LedEtat(x)		digitalRead (x)
 #define LedOn(x)		digitalWrite(x, HIGH)
 #define LedOff(x)		digitalWrite(x, LOW )
 #define LedToggle(x)		digitalWrite(x, !LedEtat(x))
+#else
+#define LedEtat(x)		((PIND & (1<<(x))) != 0)
+#define LedOn(x)		(PORTD |=  (1<<(x)))
+#define LedOff(x)		(PORTD &= ~(1<<(x)))
+#define LedToggle(x)		(PIND = (1<<(x)))
+#endif
 
-#define Led1Etat			LedEtat  (Led1Pin)
+#define Led1Etat		LedEtat  (Led1Pin)
 #define Led1On			LedOn    (Led1Pin)
 #define Led1Off			LedOff   (Led1Pin)
 #define Led1Toggle		LedToggle(Led1Pin)
-#define Led2Etat			LedEtat  (Led2Pin)
+#define Led2Etat		LedEtat  (Led2Pin)
 #define Led2On			LedOn    (Led2Pin)
 #define Led2Off			LedOff   (Led2Pin)
 #define Led2Toggle		LedToggle(Led2Pin)
-#define Led3Etat			LedEtat  (Led3Pin)
+#define Led3Etat		LedEtat  (Led3Pin)
 #define Led3On			LedOn    (Led3Pin)
 #define Led3Off			LedOff   (Led3Pin)
 #define Led3Toggle		LedToggle(Led3Pin)
-#define Led4Etat			LedEtat  (Led4Pin)
+#define Led4Etat		LedEtat  (Led4Pin)
 #define Led4On			LedOn    (Led4Pin)
 #define Led4Off			LedOff   (Led4Pin)
 #define Led4Toggle		LedToggle(Led4Pin)
-
-// LED in carte
-#define LedCartePin		LED_BUILTIN
-#define LedCarteEtat		LedEtat  (LedCartePin)
-#define LedCarteOn		LedOn    (LedCartePin)
-#define LedCarteOff		LedOff   (LedCartePin)
-#define LedCarteToggle		LedToggle(LedCartePin)
 
 // Haut-Parleur
 #define HPPin			PIN1
@@ -120,35 +100,36 @@
 #define MotGAvance		Led3Pin
 
 
+// Setup ----------------------------------------------------------------------
+void LCbot_Setup () {
 
-void LCbot_Setup () {				// Define I/O
-
-// Direction:
-//  0 : INPUT
-//  1 : OUTPUT
+    // Direction:
+    //  0 : INPUT
+    //  1 : OUTPUT
     DDRD = 0b11110010 | (DDRD & 0b1);
-//           ^------- pinMode (Led4Pin, OUTPUT);
-//            ^------ pinMode (Led3Pin, OUTPUT);
-//             ^----- pinMode (Led2Pin, OUTPUT);
-//              ^---- pinMode (Led1Pin, OUTPUT);
-//               ^--- pinMode (Pous2Pin, INPUT);
-//                ^-- pinMode (Pous1Pin, INPUT);
-//                 ^- pinMode (HPPin, OUTPUT);
+    //       ^||||||- pinMode (Led4Pin, OUTPUT);
+    //        ^|||||- pinMode (Led3Pin, OUTPUT);
+    //         ^||||- pinMode (Led2Pin, OUTPUT);
+    //          ^|||- pinMode (Led1Pin, OUTPUT);
+    //           ^||- pinMode (Pous2Pin, INPUT);
+    //            ^|- pinMode (Pous1Pin, INPUT);
+    //             ^- pinMode (HPPin, OUTPUT);
 }
 
 void LCbot_ShowStart () {			// Show LearnCbot is ready
-    pinMode (LedCartePin, OUTPUT);
+    pinMode (LED_BUILTIN, OUTPUT);
 
     delay(200);
-    LedCarteOn;
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(500);
 
     for (int i = 0; i < 3; i++) {		// 3 parpadeos
-	LedCarteOff;	delay(100);
-	LedCarteOn;	delay(100);
+	digitalWrite(LED_BUILTIN, LOW );	delay(100);
+	digitalWrite(LED_BUILTIN, HIGH);	delay(100);
     }
 
-    LedCarteOff; delay(200);			// apagar
+    digitalWrite(LED_BUILTIN, LOW );		// apagar
+    delay(200);
 }
 
 // LCbot OOP ------------------------------------------------------------------
